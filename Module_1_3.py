de9b18e8a048c794fa63b7d2bc903cc801d53fdb
MaskGleb <gnk.g1o6us@gmail.com>
2024-12-21 23:51:07 +0500

    Add files via upload

--- Module_1_3.py
+++ Module_1_3.py
@@ -1,6 +1,6 @@
 exemple="Абракадабра"
 print(exemple[0])
 print(exemple[-1])
-print(exemple[:len(exemple)//2])
+print(exemple[len(exemple)//2:len(exemple)])
 print(exemple[::-1])
 print(exemple[1::2])
